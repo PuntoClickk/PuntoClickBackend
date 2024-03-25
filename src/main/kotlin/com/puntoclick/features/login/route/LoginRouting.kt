13d5854bb923f046c6159618968336a43e698e82
Irving Gonzalez <gonzalez.irving.1994@outlook.com>
2024-03-25 16:36:38 -0500

    add login module

---
+++ src/main/kotlin/com/puntoclick/features/login/route/LoginRouting.kt
@@ -0,0 +1,11 @@
+package com.puntoclick.features.login.route
+
+import io.ktor.server.application.*
+import io.ktor.server.routing.*
+
+fun Route.loginRouting() {
+
+    route("/login") {
+
+    }
+}
