bc77b49588f284624f8822de911c2cfdb6f088c6
Diego Beltrán <diegoramms@gmail.com>
2024-03-25 14:23:49 -0500

    Configure Controllers

--- src/main/kotlin/com/puntoclick/plugins/Routing.kt
+++ src/main/kotlin/com/puntoclick/plugins/Routing.kt
@@ -1,25 +1,16 @@
 package com.puntoclick.plugins
 
-import com.puntoclick.features.roles.controller.RoleController
+import com.puntoclick.di.Controllers
 import com.puntoclick.features.roles.route.roleRouting
 import io.ktor.http.*
-import io.ktor.resources.*
 import io.ktor.server.application.*
-import io.ktor.server.http.content.*
 import io.ktor.server.plugins.statuspages.*
 import io.ktor.server.resources.*
-import io.ktor.server.resources.Resources
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import kotlinx.serialization.Serializable
-import org.koin.core.component.KoinComponent
-import org.koin.core.component.inject
-import org.koin.ktor.ext.inject
 
-fun Application.configureControllers(){
-    val roleController by inject<RoleController>()
-}
-fun Application.configureRouting() {
+
+fun Application.configureRouting(controllers: Controllers) {
 
     install(Resources)
     install(StatusPages) {
@@ -28,10 +19,7 @@ fun Application.configureRouting() {
         }
     }
     routing {
-        roleRouting(roleController)
+        roleRouting(controllers.roleController)
+
     }
 }
-
-@Serializable
-@Resource("/articles")
-class Articles(val sort: String? = "new")
