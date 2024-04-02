da9f453d309ed7dbdf7dc8d6ff4fb8e90b85918b
Diego Beltrán <diegoramms@gmail.com>
2024-04-01 22:52:17 -0500

    Generics Methods are added into FeatureUtils.kt

--- src/main/kotlin/com/puntoclick/features/utils/FeatureUtils.kt
+++ src/main/kotlin/com/puntoclick/features/utils/FeatureUtils.kt
@@ -1,7 +1,28 @@
 package com.puntoclick.features.utils
 
+import com.puntoclick.data.model.AppResponse
 import com.puntoclick.data.model.AppResult
-import com.puntoclick.data.model.createError
+import com.puntoclick.data.model.ErrorResponse
+import io.ktor.http.*
+
+
+fun  <T: Any>AppResult<T>.handleResult() : AppResponse<T> {
+    return when(this){
+        is AppResult.Success -> AppResponse(data)
+        is AppResult.Error -> { AppResponse(error = errorResponse)
+        }
+    }
+}
+
+fun createError(title: String?, description: String?, status: HttpStatusCode = HttpStatusCode.BadRequest): AppResult.Error {
+    return AppResult.Error(
+        ErrorResponse(
+            title =  title ?: "",
+            description = description ?: ""
+        ),
+        appStatus = status
+    )
+}
 
 
 inline fun <T: Any> tryCatch( block: () -> AppResult<T>): AppResult<T> {
