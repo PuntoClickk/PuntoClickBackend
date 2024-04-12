60b13bb0b4dfba1a7659faa230d86752900a5590
Diego Beltrán <diegoramms@gmail.com>
2024-04-12 17:28:02 -0600

    Remove unused code

--- src/main/kotlin/com/puntoclick/Application.kt
+++ src/main/kotlin/com/puntoclick/Application.kt
@@ -3,11 +3,8 @@ package com.puntoclick
 
 import com.puntoclick.data.database.configureDatabase
 import com.puntoclick.plugins.*
-import com.puntoclick.security.AppEncryption
 import io.ktor.server.application.*
 import io.ktor.server.netty.*
-import org.koin.core.qualifier.named
-import org.koin.ktor.ext.inject
 
 fun main(args: Array<String>): Unit = EngineMain.main(args)
 
@@ -20,14 +17,6 @@ fun Application.module() {
     configureSerialization()
     configureSecurity()
     configureRouting()
-
-    val plainText = "Hello world"
-    val appEncryption by inject<AppEncryption>(named(name = "AES"))
-    val encrypted = appEncryption.encrypt(plainText)
-    println(encrypted)
-    println(appEncryption.decrypt(encrypted))
-    println(appEncryption.decrypt("E0F5rdzdYhlRUqnh1hgRgiVNhpWOHTW7NVoq:kLQqTLzP5OtqfctM"))
-
 }
 
 
