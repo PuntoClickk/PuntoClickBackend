4f2759112a2e81f459c613822fee79dba6da7ffe
Diego Beltrán <diegoramms@gmail.com>
2024-04-12 17:25:51 -0600

    Format

--- src/main/kotlin/com/puntoclick/security/AppEncryptionAES.kt
+++ src/main/kotlin/com/puntoclick/security/AppEncryptionAES.kt
@@ -11,22 +11,21 @@ class AppEncryptionAES: AppEncryption {
     private val key = System.getenv()["PASSWORD_ENC"].orEmpty()
 
     override fun encrypt(plainText: String): String {
-        println(key)
         val cipher = Cipher.getInstance("AES/GCM/NoPadding")
-        val decodedKey = Base64.getDecoder().decode(key)
+        val decodedKey = Base64.getMimeDecoder().decode(key)
         val secretKey: SecretKey = SecretKeySpec(decodedKey, "AES")
         cipher.init(Cipher.ENCRYPT_MODE, secretKey)
         val iv = cipher.iv
         val encryptedBytes = cipher.doFinal(plainText.toByteArray())
-        return Base64.getEncoder().encodeToString(encryptedBytes) + ":" + Base64.getEncoder().encodeToString(iv)
+        return Base64.getMimeEncoder().encodeToString(encryptedBytes) + ":" + Base64.getEncoder().encodeToString(iv)
     }
 
     override fun decrypt(encryptedText: String): String {
         val parts = encryptedText.split(":")
-        val encryptedData = Base64.getDecoder().decode(parts[0])
-        val iv = Base64.getDecoder().decode(parts[1])
+        val encryptedData = Base64.getMimeDecoder().decode(parts[0])
+        val iv = Base64.getMimeDecoder().decode(parts[1])
         val cipher = Cipher.getInstance("AES/GCM/NoPadding")
-        val decodedKey = Base64.getDecoder().decode(key)
+        val decodedKey = Base64.getMimeDecoder().decode(key)
         val secretKey: SecretKey = SecretKeySpec(decodedKey, "AES")
         val gcmParameterSpec = GCMParameterSpec(128, iv)
         cipher.init(Cipher.DECRYPT_MODE, secretKey, gcmParameterSpec)
