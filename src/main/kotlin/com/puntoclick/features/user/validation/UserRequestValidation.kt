e00aed774f116e1ce0d4cf0a94a031d10a3338c2
Diego Beltrán <diegoramms@gmail.com>
2024-04-25 22:19:52 -0500

    new method is used

--- src/main/kotlin/com/puntoclick/features/user/validation/UserRequestValidation.kt
+++ src/main/kotlin/com/puntoclick/features/user/validation/UserRequestValidation.kt
@@ -2,15 +2,15 @@ package com.puntoclick.features.user.validation
 
 import com.puntoclick.features.user.model.CreateUserRequest
 import com.puntoclick.features.utils.isValidEmail
-import com.puntoclick.features.utils.validateRequestString
+import com.puntoclick.features.utils.validateStringRequest
 import io.ktor.server.plugins.requestvalidation.*
 
 
 
 fun CreateUserRequest.validateCreateUserRequest (): ValidationResult {
         return when {
-            lastName.validateRequestString().isNullOrBlank() -> ValidationResult.Invalid("Error Name")
-            name.validateRequestString().isNullOrBlank() -> ValidationResult.Invalid("Error Name")
+            !lastName.validateStringRequest() -> ValidationResult.Invalid("Error Name")
+            !name.validateStringRequest() -> ValidationResult.Invalid("Error Name")
             isValidEmail(email) -> ValidationResult.Invalid("Error Email")
             else -> ValidationResult.Valid
         }
