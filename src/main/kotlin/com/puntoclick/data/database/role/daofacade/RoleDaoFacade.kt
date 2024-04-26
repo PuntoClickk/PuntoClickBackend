91b54bd502f462a5e1e1069c52830c5fe71aff0b
Irving Gonzalez <gonzalez.irving.1994@outlook.com>
2024-04-25 20:27:03 -0500

    add request classe to handle request and update in endpoints

--- src/main/kotlin/com/puntoclick/data/database/role/daofacade/RoleDaoFacade.kt
+++ src/main/kotlin/com/puntoclick/data/database/role/daofacade/RoleDaoFacade.kt
@@ -1,12 +1,14 @@
 package com.puntoclick.data.database.role.daofacade
 
 import com.puntoclick.data.database.entity.Role
+import com.puntoclick.features.roles.model.CreateRoleRequest
+import com.puntoclick.features.roles.model.UpdateRoleRequest
 import java.util.UUID
 
 interface RoleDaoFacade {
     suspend fun allRoles(): List<Role>
     suspend fun role(uuid: UUID): Role?
-    suspend fun addRole(name: String): Boolean
-    suspend fun updateRole(role: Role): Boolean
+    suspend fun addRole(createRoleRequest: CreateRoleRequest): Boolean
+    suspend fun updateRole(updateRoleRequest: UpdateRoleRequest): Boolean
     suspend fun deleteRole(uuid: UUID): Boolean
 }
---
+++ src/main/kotlin/com/puntoclick/features/team/model/CreateTeamRequest.kt
@@ -0,0 +1,8 @@
+package com.puntoclick.features.team.model
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+class CreateTeamRequest(
+    val name: String
+)
