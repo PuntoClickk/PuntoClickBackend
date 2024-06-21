7e26ef391c9b4768eee0b33c8770a74e25610ce4
Diego Beltrán <diegoramms@gmail.com>
2024-06-21 15:43:27 -0500

    size is changed

--- src/main/kotlin/com/puntoclick/data/database/invitation/table/InvitationsTable.kt
+++ src/main/kotlin/com/puntoclick/data/database/invitation/table/InvitationsTable.kt
@@ -7,7 +7,7 @@ import org.jetbrains.exposed.sql.Table
 object InvitationTable : Table("invitation") {
     val uuid = uuid("id").autoGenerate().uniqueIndex()
     val team = reference("team", TeamTable.uuid)
-    val code = varchar("code", 255).uniqueIndex()
+    val code = varchar("code", 11).uniqueIndex()
     val createdAt = long("created_at").default(System.currentTimeMillis())
     val updatedAt = long("updated_at").default(System.currentTimeMillis())
     val expiresAt = long("expires_at")
