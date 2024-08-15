e3aa7fca06b13ee0d4f70223eef8681c9ee8c31f
Diego Beltrán <diegoramms@gmail.com>
2024-08-15 17:46:12 -0500

    register is removed

--- src/main/kotlin/com/puntoclick/features/permission/route/PermissionRouting.kt
+++ src/main/kotlin/com/puntoclick/features/permission/route/PermissionRouting.kt
@@ -9,11 +9,9 @@ import com.puntoclick.data.utils.USER_IDENTIFIER
 import com.puntoclick.features.permission.controller.PermissionController
 import com.puntoclick.features.utils.handleResult
 import com.puntoclick.features.utils.retrieveLocale
-import com.puntoclick.plugins.LimitType
 import com.puntoclick.plugins.getIdentifier
 import com.puntoclick.security.AppEncryption
 import io.ktor.server.application.*
-import io.ktor.server.plugins.ratelimit.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
@@ -26,44 +24,42 @@ fun Route.permissionRouting() {
     val appEncryption by inject<AppEncryption>(qualifier = named("AES"))
 
     route("/permission") {
-        rateLimit(RateLimitName(LimitType.PROTECTED.value)) {
-            post("/add") {
-                val request = call.receive<AddPermissionRequest>()
-                val userId = call.getIdentifier(appEncryption, USER_IDENTIFIER)
-                val teamId = call.getIdentifier(appEncryption, TEAM_IDENTIFIER)
-                val locale = call.retrieveLocale()
-                val result = permissionController.addPermission(userId, teamId, locale, request)
-                call.respond(message = result.handleResult(), status = result.status)
-            }
-            post("/update") {
-                val request = call.receive<UpdatePermissionRequest>()
-                val userId = call.getIdentifier(appEncryption, USER_IDENTIFIER)
-                val teamId = call.getIdentifier(appEncryption, TEAM_IDENTIFIER)
-                val locale = call.retrieveLocale()
-                val result = permissionController.updatePermission(userId, teamId, locale, request)
-                call.respond(message = result.handleResult(), status = result.status)
-            }
+        post("/add") {
+            val request = call.receive<AddPermissionRequest>()
+            val userId = call.getIdentifier(appEncryption, USER_IDENTIFIER)
+            val teamId = call.getIdentifier(appEncryption, TEAM_IDENTIFIER)
+            val locale = call.retrieveLocale()
+            val result = permissionController.addPermission(userId, teamId, locale, request)
+            call.respond(message = result.handleResult(), status = result.status)
+        }
+        post("/update") {
+            val request = call.receive<UpdatePermissionRequest>()
+            val userId = call.getIdentifier(appEncryption, USER_IDENTIFIER)
+            val teamId = call.getIdentifier(appEncryption, TEAM_IDENTIFIER)
+            val locale = call.retrieveLocale()
+            val result = permissionController.updatePermission(userId, teamId, locale, request)
+            call.respond(message = result.handleResult(), status = result.status)
+        }
 
-            post("/permissions") {
-                val teamId = call.getIdentifier(appEncryption, TEAM_IDENTIFIER)
-                val result = permissionController.getPermissionByTeam(teamId)
-                call.respond(message = result.handleResult(), status = result.status)
-            }
+        post("/permissions") {
+            val teamId = call.getIdentifier(appEncryption, TEAM_IDENTIFIER)
+            val result = permissionController.getPermissionByTeam(teamId)
+            call.respond(message = result.handleResult(), status = result.status)
+        }
 
-            post("/permissions/role") {
-                val teamId = call.getIdentifier(appEncryption, TEAM_IDENTIFIER)
-                val roleId = call.getIdentifier(appEncryption, ROLE_IDENTIFIER)
-                val result = permissionController.getPermissionsByRoleAndTeam(roleId, teamId)
-                call.respond(message = result.handleResult(), status = result.status)
-            }
+        post("/permissions/role") {
+            val teamId = call.getIdentifier(appEncryption, TEAM_IDENTIFIER)
+            val roleId = call.getIdentifier(appEncryption, ROLE_IDENTIFIER)
+            val result = permissionController.getPermissionsByRoleAndTeam(roleId, teamId)
+            call.respond(message = result.handleResult(), status = result.status)
+        }
 
-            post("/delete") {
-                val request = call.receive<DeletePermissionRequest>()
-                val userId = call.getIdentifier(appEncryption, USER_IDENTIFIER)
-                val locale = call.retrieveLocale()
-                val result = permissionController.deletePermission(userId, locale, request)
-                call.respond(message = result.handleResult(), status = result.status)
-            }
+        post("/delete") {
+            val request = call.receive<DeletePermissionRequest>()
+            val userId = call.getIdentifier(appEncryption, USER_IDENTIFIER)
+            val locale = call.retrieveLocale()
+            val result = permissionController.deletePermission(userId, locale, request)
+            call.respond(message = result.handleResult(), status = result.status)
         }
     }
 }
