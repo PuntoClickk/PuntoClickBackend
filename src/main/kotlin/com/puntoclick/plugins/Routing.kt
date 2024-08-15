c2a8781af31c79b155cbd031c7016d4785316888
Diego Beltrán <diegoramms@gmail.com>
2024-08-15 17:35:15 -0500

    Validations by env are added

--- src/main/kotlin/com/puntoclick/plugins/Routing.kt
+++ src/main/kotlin/com/puntoclick/plugins/Routing.kt
@@ -1,5 +1,6 @@
 package com.puntoclick.plugins
 
+import com.puntoclick.AppEnvironment
 import com.puntoclick.data.model.ErrorResponse
 import com.puntoclick.features.auth.route.authRouting
 import com.puntoclick.features.category.route.categoryRouting
@@ -11,6 +12,7 @@ import com.puntoclick.features.store.routing.storeRouting
 import com.puntoclick.features.team.route.teamRouting
 import com.puntoclick.features.user.route.userRouting
 import com.puntoclick.features.utils.*
+import com.puntoclick.getEnvironment
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
@@ -24,28 +26,40 @@ import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
 
+    val env = getEnvironment()
     val jwtParams = getJWTParams()
 
     install(Resources)
     install(StatusPages) {
         exception<Throwable> { call, cause ->
-            call.respond(
-                message = ErrorResponse("Error", cause.message ?: "Error App"),
-                status = HttpStatusCode.BadRequest
-            )
+            val locale = call.retrieveLocale()
+            if (env == AppEnvironment.DEV)
+                call.respond(
+                    message = ErrorResponse("Error", cause.message ?: "Error App"),
+                    status = HttpStatusCode.BadRequest
+                )
+            else locale.createError()
         }
         exception<RequestValidationException> { call, cause ->
-            call.respond(
-                message = ErrorResponse("Error", cause.reasons.getOrNull(0) ?: "Error App"),
-                status = HttpStatusCode.BadRequest
-            )
+            val locale = call.retrieveLocale()
+            if (env == AppEnvironment.DEV)
+                call.respond(
+                    message = ErrorResponse("Error", cause.reasons.getOrNull(0) ?: "Error App"),
+                    status = HttpStatusCode.BadRequest
+                )
+            else locale.createError()
         }
 
         status(HttpStatusCode.TooManyRequests) { call, status ->
             val locale = call.retrieveLocale()
             val retryAfter = call.response.headers["Retry-After"]
-            println("429: Too many requests. Wait for $retryAfter seconds.")
-            call.respond(
+
+            if (env == AppEnvironment.DEV)
+                call.respond(
+                    message = ErrorResponse("Error", "429: Too many requests. Wait for $retryAfter seconds."),
+                    status = HttpStatusCode.BadRequest
+                )
+            else call.respond(
                 locale.createError(
                     descriptionKey = StringResourcesKey.TOO_MANY_REQUESTS_ERROR_KEY,
                     status = status
